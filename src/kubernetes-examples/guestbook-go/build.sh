cb1de7f5829c75749c1be288bb2deaf544015776
Justin SB <justinsb@google.com>
2023-01-24 14:35:02 -0500

    Create kubernetes-examples/guestbook-go/v3.0.20230124

---
+++ src/kubernetes-examples/guestbook-go/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# cd to the repo root
+REPO_ROOT=$(git rev-parse --show-toplevel)
+cd "${REPO_ROOT}"
+
+VERSION=v3.0.20230124
+
+mkdir -p kubernetes-examples/guestbook-go/${VERSION}/
+# TODO: Copy from somewhere
+
+kpt pkg init kubernetes-examples/guestbook-go/${VERSION}/ --description "guestbook-go"
+cat > kubernetes-examples/guestbook-go/${VERSION}/README.md <<EOF
+# guestbook-go
+
+## Description
+guestbook-go is one of the kubernetes examples, a version of the guestbook example application written in go.
+
+https://github.com/kubernetes/examples/tree/master/guestbook-go
+EOF
